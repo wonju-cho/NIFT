97148894f62621edd2b6c1f171d5ab12908c7e92
ansdudals8 <ansdudals8@naver.com>
2025-03-14 12:43:33 +0900

    Chore: api 경로 수정

--- frontend/src/app/api/kakao/route.js
+++ frontend/src/app/api/kakao/route.js
@@ -2,7 +2,7 @@ export async function POST(req) {
   try {
     const { accessToken } = await req.json();
     
-    const response = await fetch("http://localhost:8080/api/users/login", {
+    const response = await fetch("http://localhost:8080/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
