a3634d661186b9904ffcc681c1a17cf6e5d6f132
조희진 <allana123@naver.com>
2025-03-19 10:17:20 +0900

    fix: 깃 충돌 해결 (#25)

--- smart-contract/migrations/1_initial_migration.js
+++ smart-contract/migrations/1_initial_migration.js
@@ -1,12 +1,7 @@
 const Migrations = artifacts.require("Migrations");
 
-<<<<<<< HEAD
 module.exports = async function (deployer) {
   await deployer.deploy(Migrations);
   const instance = await Migrations.deployed();
   console.log("✅ Migrations deployed at:", instance.address);
-=======
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
->>>>>>> 0d4bb62 (Feat: init smart-contract)
 };
