bb3502145705b8c74a62abc817ae5bd601c20055
Kimdowon <dowon999@naver.com>
2025-03-24 17:38:40 +0900

    Feat: gifticon entity 추가, Users  entity ì 컬럼  수정

--- backend/src/main/java/com/e101/nift/gifticon/entity/Gifticon.java
+++ backend/src/main/java/com/e101/nift/gifticon/entity/Gifticon.java
@@ -20,9 +20,9 @@ import java.util.List;
 public class Gifticon {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long gifticon_id;
+    private Long gifticonId;
     
-    private String gifticon_title;
+    private String gifticonTitle;
     private String description;
     private Float price;
     private String imageUrl;
@@ -35,6 +35,6 @@ public class Gifticon {
     @JoinColumn(name = "brand_id", nullable = false)
     private Brand brand;
 
-    @OneToMany(mappedBy = "gifticon", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "gifticon_id", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Article> articles;
 }
