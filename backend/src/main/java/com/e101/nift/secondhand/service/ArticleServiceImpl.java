f6ff0d3df5aa1315f5da9fd7ca0a3243320dd9f0
Kimdowon <dowon999@naver.com>
2025-03-26 12:31:02 +0900

    Fix: setExpirationDate 타입 -> LocalDataTime

--- backend/src/main/java/com/e101/nift/secondhand/service/ArticleServiceImpl.java
+++ backend/src/main/java/com/e101/nift/secondhand/service/ArticleServiceImpl.java
@@ -19,6 +19,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
