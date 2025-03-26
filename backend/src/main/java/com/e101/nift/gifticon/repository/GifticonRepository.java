0384a3118c03826dcd64badae7de2d8b9bfe2a17
Kimdowon <dowon999@naver.com>
2025-03-26 09:42:22 +0900

    GifticonRepository_findById 수정

--- backend/src/main/java/com/e101/nift/gifticon/repository/GifticonRepository.java
+++ backend/src/main/java/com/e101/nift/gifticon/repository/GifticonRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface GifticonRepository extends JpaRepository<Gifticon, Long> {
-    Optional<Gifticon> findById(Long kakaoId);
+    Optional<Gifticon> findById(Long gifticonId);
 }
