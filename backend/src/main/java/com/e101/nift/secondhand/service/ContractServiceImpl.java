6d929e73b7c51d2afdc8056352980b07a59e5423
조희진 <allana123@naver.com>
2025-03-28 09:34:05 +0900

    feat: 블록체인 트랜잭션 조회 기본 구조 구축 (#25, #S12P21E101-271)

--- backend/src/main/java/com/e101/nift/secondhand/service/ContractServiceImpl.java
+++ backend/src/main/java/com/e101/nift/secondhand/service/ContractServiceImpl.java
@@ -10,23 +10,32 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ContractServiceImpl implements ContractService {
     private final ArticleHistoryRepository articleHistoryRepository;
     private final ArticleRepository articleRepository;
+    private final TransactionService transactionService;
 
     @Override
-    public void addArticleHistory(Long articleId, Long userId) {
+    public void addArticleHistory(Long articleId, String txHash, Long userId) {
         articleRepository.findById(articleId).orElseThrow(() -> new ArticleException(ArticleErrorCode.ARTICLE_NOT_FOUND));
 
-        articleHistoryRepository.save(
-                ArticleHistory.builder()
-                        .articleId(articleId)
-                        .historyType(ContractType.PURCHASE.getType())
-                        .userId(userId)
-                        .build()
-        );
+        try {
+            String status = transactionService.getTxStatus(txHash);
+
+            articleHistoryRepository.save(
+                    ArticleHistory.builder()
+                            .articleId(articleId)
+                            .historyType(ContractType.PURCHASE.getType())
+                            .userId(userId)
+                            .build()
+            );
+
+        } catch (IOException e) {
+        }
     }
 }
