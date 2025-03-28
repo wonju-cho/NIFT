fea725cbaebfd9d957614c21b710d5781d2e0997
조희진 <allana123@naver.com>
2025-03-28 09:59:52 +0900

    fix: 트랜잭션 로그 들고 오기 추가 (#25, #S12P21E101-271)

--- backend/src/main/java/com/e101/nift/secondhand/service/TransactionServiceImpl.java
+++ backend/src/main/java/com/e101/nift/secondhand/service/TransactionServiceImpl.java
@@ -6,10 +6,12 @@ import com.e101.nift.secondhand.model.state.ContractStatus;
 import org.springframework.stereotype.Service;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.EthGetTransactionReceipt;
+import org.web3j.protocol.core.methods.response.Log;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -33,8 +35,16 @@ public class TransactionServiceImpl implements TransactionService{
     @Override
     public String getTxStatus(String txHash) {
         Optional<TransactionReceipt> receiptOpt = getTransactionReceipt(txHash);
-        return receiptOpt.filter(TransactionReceipt::isStatusOK)
+        return receiptOpt
+                .filter(TransactionReceipt::isStatusOK)
                 .map(receipt -> ContractStatus.SUCCESS.getType())
-                .orElse(ContractStatus.FAILED.getType());
+                .orElseThrow(() -> new ArticleException(ArticleErrorCode.UNPROCESSABLE_TRANSACTION));
+    }
+
+    @Override
+    public List<Log> getTransactionLogs(String txHash) {
+        return getTransactionReceipt(txHash)
+                .map(TransactionReceipt::getLogs)
+                .orElseThrow(() -> new ArticleException(ArticleErrorCode.UNPROCESSABLE_TRANSACTION));
     }
 }
