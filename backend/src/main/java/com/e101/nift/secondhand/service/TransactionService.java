1ced64011e59a20ba5dfb9cf0138825b935ae2df
조희진 <allana123@naver.com>
2025-03-28 09:42:44 +0900

    fix: 트랜잭션 조회 기본 구조 변경 (#25, #S12P21E101-271)

--- backend/src/main/java/com/e101/nift/secondhand/service/TransactionService.java
+++ backend/src/main/java/com/e101/nift/secondhand/service/TransactionService.java
@@ -2,10 +2,9 @@ package com.e101.nift.secondhand.service;
 
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
-import java.io.IOException;
 import java.util.Optional;
 
 public interface TransactionService {
-    Optional<TransactionReceipt> getTransactionReceipt(String txHash) throws IOException;
-    String getTxStatus(String txHash) throws IOException;
+    Optional<TransactionReceipt> getTransactionReceipt(String txHash);
+    String getTxStatus(String txHash);
 }
