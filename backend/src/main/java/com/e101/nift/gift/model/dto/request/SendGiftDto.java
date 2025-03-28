720e9151ead162620712b2c3077467497fea5858
ansdudals8 <ansdudals8@naver.com>
2025-03-28 13:58:04 +0900

    Chore: 선물하기 API 수정 - gift-histories 추가 - 중고거래일 경우 article-histories 추가 - 중고거래일 경우 article.issold 업데이트 JIRA : S12P21E101-305

--- backend/src/main/java/com/e101/nift/gift/model/dto/request/SendGiftDto.java
+++ backend/src/main/java/com/e101/nift/gift/model/dto/request/SendGiftDto.java
@@ -11,6 +11,12 @@ import lombok.Setter;
 @AllArgsConstructor
 public class SendGiftDto {
     private Long toUserKakaoId;
+    // 증고거래에서 선물 -> articleId가 담길 것
+    // 마이페이지에서 선물 -> gifticonId가 담길 것
     private Long gifticonId;
     private String mongoId;
+
+    // 중고거래에서 선물 -> article
+    // 마이페이지에서 선물 -> gifticon
+    private String type;
 }
