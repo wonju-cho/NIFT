0d13a4f2ef0cfc3ca4392d3a6d2b97dbdbe11596
조희진 <allana123@naver.com>
2025-03-28 16:08:23 +0900

    fix: 트랜잭션 이벤트 로그 가져오기 완성 (#25, #S12P21E101-271)

--- backend/src/main/java/com/e101/nift/secondhand/service/ContractServiceImpl.java
+++ backend/src/main/java/com/e101/nift/secondhand/service/ContractServiceImpl.java
@@ -3,13 +3,13 @@ package com.e101.nift.secondhand.service;
 import com.e101.nift.secondhand.entity.ArticleHistory;
 import com.e101.nift.secondhand.exception.ArticleErrorCode;
 import com.e101.nift.secondhand.exception.ArticleException;
+import com.e101.nift.secondhand.model.contract.GifticonNFT;
 import com.e101.nift.secondhand.model.state.ContractType;
 import com.e101.nift.secondhand.repository.ArticleHistoryRepository;
 import com.e101.nift.secondhand.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.web3j.protocol.core.methods.response.Log;
 
 import java.util.List;
 
@@ -25,7 +25,9 @@ public class ContractServiceImpl implements ContractService {
     public void addArticleHistory(Long articleId, String txHash, Long userId) {
         articleRepository.findById(articleId).orElseThrow(() -> new ArticleException(ArticleErrorCode.ARTICLE_NOT_FOUND));
 
-        List<Log> logs = transactionService.getTransactionLogs(txHash);
+        List<GifticonNFT.NFTPurchasedEventResponse> logs = transactionService.getPurchaseEventsByTxHash(txHash);
+
+        log.info("[ContractService] 트랜잭션 로그 확인: {}",logs);
 
         articleHistoryRepository.save(
                 ArticleHistory.builder()
--- backend/src/main/java/com/e101/nift/secondhand/service/TransactionService.java
+++ backend/src/main/java/com/e101/nift/secondhand/service/TransactionService.java
@@ -1,5 +1,6 @@
 package com.e101.nift.secondhand.service;
 
+import com.e101.nift.secondhand.model.contract.GifticonNFT;
 import org.web3j.protocol.core.methods.response.Log;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
@@ -10,4 +11,5 @@ public interface TransactionService {
     Optional<TransactionReceipt> getTransactionReceipt(String txHash);
     String getTxStatus(String txHash);
     List<Log> getTransactionLogs(String txHash);
+    List<GifticonNFT.NFTPurchasedEventResponse> getPurchaseEventsByTxHash(String txHash);
 }
