1e7c8b6ee2cd379ac2d5247ecd5f67b4bdb32bdf
Wonju Jo <68497276+wonju-cho@users.noreply.github.com>
2025-04-01 15:38:32 +0900

    Fix/.env에서 받아오는거 id맞게 수정

--- backend/src/main/java/com/e101/nift/common/config/DotenvConfig.java
+++ backend/src/main/java/com/e101/nift/common/config/DotenvConfig.java
@@ -18,6 +18,6 @@ public class DotenvConfig {
     @PostConstruct
     public void init() {
         Dotenv dotenv = Dotenv.load();
-        System.setProperty("web3j.private.key", dotenv.get("PRIVATE_KEY"));
+        System.setProperty("web3j.private.key", dotenv.get("WEB3J_PRIVATE_KEY"));
     }
 }
