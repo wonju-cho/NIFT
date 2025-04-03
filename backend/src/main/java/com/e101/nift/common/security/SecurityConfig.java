4adc20f1a80275159f114c8bf3d0831d0949a02b
조희진 <allana123@naver.com>
2025-04-03 22:26:56 +0900

    hotfix: security에 domain 추가

--- backend/src/main/java/com/e101/nift/common/security/SecurityConfig.java
+++ backend/src/main/java/com/e101/nift/common/security/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3001", "https://j12e101.p.ssafy.io/", "https://j12e101.p.ssafy.io:3001/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3001", "https://j12e101.p.ssafy.io", "https://j12e101.p.ssafy.io:3001"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
