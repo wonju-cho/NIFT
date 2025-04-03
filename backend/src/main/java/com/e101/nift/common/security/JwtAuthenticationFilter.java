4b8db119d6242e5c3bb3ce69fe0d5e46d3418de5
조희진 <allana123@naver.com>
2025-04-03 22:46:57 +0900

    hotfix: 토큰 필터 log 추가

--- backend/src/main/java/com/e101/nift/common/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/e101/nift/common/security/JwtAuthenticationFilter.java
@@ -4,14 +4,11 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -38,6 +35,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             }
 
             String token = resolveToken(request);
+            log.info("[JwtAuthenticationFilter] token {}", token);
             if (token != null) {
                 if (!jwtTokenProvider.validateToken(token)) {
                     throw new IllegalArgumentException("유효하지 않은 토큰입니다.");
