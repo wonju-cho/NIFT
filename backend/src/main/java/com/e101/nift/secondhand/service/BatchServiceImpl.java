0bed61fd9f8493103e6fc485948cb9c50953d4a1
조희진 <allana123@naver.com>
2025-04-04 10:40:24 +0900

    feat: 선물 받기 배치 실행 (#107, #S12P21E101-391)

--- backend/src/main/java/com/e101/nift/secondhand/service/BatchServiceImpl.java
+++ backend/src/main/java/com/e101/nift/secondhand/service/BatchServiceImpl.java
@@ -5,6 +5,8 @@ import com.e101.nift.gift.entity.GiftHistory;
 import com.e101.nift.gift.repository.GiftHistoryRepository;
 import com.e101.nift.secondhand.entity.Article;
 import com.e101.nift.secondhand.entity.SyncStatus;
+import com.e101.nift.secondhand.exception.ArticleErrorCode;
+import com.e101.nift.secondhand.exception.ArticleException;
 import com.e101.nift.secondhand.model.contract.GifticonNFT;
 import com.e101.nift.secondhand.model.state.SaleStatus;
 import com.e101.nift.secondhand.model.state.SyncType;
@@ -12,7 +14,6 @@ import com.e101.nift.secondhand.repository.ArticleHistoryRepository;
 import com.e101.nift.secondhand.repository.ArticleRepository;
 import com.e101.nift.secondhand.repository.SyncStatusRepository;
 import com.e101.nift.user.entity.User;
-import com.e101.nift.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -21,6 +22,7 @@ import org.springframework.stereotype.Service;
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -31,7 +33,6 @@ public class BatchServiceImpl implements BatchService {
     private final ArticleRepository articleRepository;
     private final GiftHistoryRepository giftHistoryRepository;
     private final ContractService contractService;
-    private final UserService userService;
     private final SyncStatusRepository syncStatusRepository;
 
     @Scheduled(fixedDelay = 90000)
@@ -52,11 +53,12 @@ public class BatchServiceImpl implements BatchService {
 
         log.info("최근 10개 블록: {}", blockNumbers);
 
-        for(BigInteger block : blockNumbers) {
+        for (BigInteger block : blockNumbers) {
             try {
                 handlePurchaseEvent(block);
                 handleListForSaleEvent(block);
                 handleGiftPendingEvent(block);
+                handleGiftedEvent(block);
                 syncStatusRepository.updateByLastSyncedBlock(block.longValue(), LocalDateTime.now(), SyncType.REAL_TIME);
             } catch (Exception e) {
                 log.error("블록 처리 실패: {}", block, e);
@@ -65,33 +67,64 @@ public class BatchServiceImpl implements BatchService {
         }
     }
 
+    private void handleGiftedEvent(BigInteger block) {
+        List<GifticonNFT.GiftPendingEventResponse> giftPendingEventResponseList = transactionService.getGiftPendingEventByBlockNumber(block);
+
+        giftPendingEventResponseList.parallelStream().forEach(response -> {
+            log.info("[BatchService] response: {}", response);
+            if (response == null) return;
+
+            try {
+                Optional<GiftHistory> optionalGiftHistory = giftHistoryRepository.findGiftHistoriesByTxHash(response.log.getTransactionHash());
+
+                if (optionalGiftHistory.isEmpty()) {
+                    log.error("[BatchService] Gifted에 대해 GiftHistory가 존재하지 않습니다 : {}", response.log.getTransactionHash());
+                    return;
+                }
+
+                GiftHistory giftHistory = optionalGiftHistory
+                        .filter(gift -> Long.valueOf(response.aliasName).equals(gift.getToUserKakaoId()))
+                        .orElseThrow(() -> new ArticleException(ArticleErrorCode.TRANSACTION_EXCEPTION));
+
+                if (giftHistory.isReceived()) return;
+
+                giftHistory.setReceived(true);
+                giftHistoryRepository.save(giftHistory);
+
+            } catch (Exception e) {
+                log.error("[BatchService] Failed to process handleGiftedEvent txHash: {}", response.log.getTransactionHash(), e);
+            }
+        });
+    }
+
+
     private void handleGiftPendingEvent(BigInteger block) {
         List<GifticonNFT.GiftPendingEventResponse> giftPendingEventResponseList = transactionService.getGiftPendingEventByBlockNumber(block);
 
         giftPendingEventResponseList.parallelStream().forEach(response -> {
             log.info("[BatchService] response: {}", response);
-            if(response == null) return;
+            if (response == null) return;
             try {
-                if(giftHistoryRepository.findGiftHistoriesByTxHash(response.log.getTransactionHash()).isEmpty()) {
+                if (giftHistoryRepository.findGiftHistoriesByTxHash(response.log.getTransactionHash()).isEmpty()) {
                     log.info("[BatchService] DB에 저장되지 않은 Hash 값: {}", response.log.getTransactionHash());
 
                     User receiver = transactionService.getUserByKaKaoId(response.aliasName);
 
                     giftHistoryRepository.save(
                             GiftHistory.builder()
-                            .fromUserId(transactionService.getUser(response.sender))
-                            .toUserId(receiver.getUserId())
-                            .toUserKakaoId(receiver.getKakaoId())
-                            .gifticon(transactionService.getGifticon(response.tokenId))
-                            .mongoId("67eb5540252c9d3711faf55b") // default card ID
-                            .isReceived(false)
-                            .createdAt(ConvertUtil.convertTimestampToLocalTime(response.transactionTime))
-                            .txHash(response.log.getTransactionHash())
-                            .build()
+                                    .fromUserId(transactionService.getUser(response.sender))
+                                    .toUserId(receiver.getUserId())
+                                    .toUserKakaoId(receiver.getKakaoId())
+                                    .gifticon(transactionService.getGifticon(response.tokenId))
+                                    .mongoId("67eb5540252c9d3711faf55b") // default card ID
+                                    .isReceived(false)
+                                    .createdAt(ConvertUtil.convertTimestampToLocalTime(response.transactionTime))
+                                    .txHash(response.log.getTransactionHash())
+                                    .build()
                     );
                 }
             } catch (Exception e) {
-                log.error("[BatchService] Failed to process handleListForSaleEvent txHash: {}", response.log.getTransactionHash(), e);
+                log.error("[BatchService] Failed to process handleGiftPendingEvent txHash: {}", response.log.getTransactionHash(), e);
             }
         });
     }
@@ -101,9 +134,9 @@ public class BatchServiceImpl implements BatchService {
 
         listedForSaleEventResponses.parallelStream().forEach(response -> {
             log.info("response: {}", response);
-            if(response == null) return;
+            if (response == null) return;
             try {
-                if(articleRepository.findArticleByTxHash(response.log.getTransactionHash()).isEmpty()) {
+                if (articleRepository.findArticleByTxHash(response.log.getTransactionHash()).isEmpty()) {
                     log.debug("[BatchService] DB에 저장되지 않은 Hash 값: {}", response.log.getTransactionHash());
 
                     articleRepository.save(
@@ -134,9 +167,9 @@ public class BatchServiceImpl implements BatchService {
         List<GifticonNFT.NFTPurchasedEventResponse> purchasedEventResponseList = transactionService.getPurchaseEventsByBlockNumber(block);
 
         purchasedEventResponseList.parallelStream().forEach(response -> {
-            if(response == null) return;
+            if (response == null) return;
             try {
-                if(articleHistoryRepository.findByTxHash(response.log.getTransactionHash()).isEmpty()) {
+                if (articleHistoryRepository.findByTxHash(response.log.getTransactionHash()).isEmpty()) {
                     log.debug("[BatchService] DB에 저장되지 않은 Hash 값: {}", response.log.getTransactionHash());
                     contractService.addArticleHistory(
                             response.serialNumber.longValue(),
@@ -149,5 +182,4 @@ public class BatchServiceImpl implements BatchService {
             }
         });
     }
-
 }
