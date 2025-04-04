49cb7e1b1eec37705f6939e4bf73d603f74eb790
문영민 <ansdudals8@naver.com>
2025-04-04 11:27:25 +0900

    #103/hotFix/categories

--- admin/next.config.js
+++ admin/next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
       { protocol: "http", hostname: "img1.kakaocdn.net" },
       { protocol: "http", hostname: "t1.kakaocdn.net" },
       { protocol: "https", hostname: "k.kakaocdn.net" },
+      { protocol: "http", hostname: "k.kakaocdn.net" },
       { protocol: "https", hostname: "sitem.ssgcdn.com" },
       { protocol: "https", hostname: "static.megamart.com" },
       { protocol: "https", hostname: "ipfs.io" },
